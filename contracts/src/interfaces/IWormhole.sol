648d88833a18cf66031c0ae6933c1e5555772bcf
bissaka <83520695+bissaka@users.noreply.github.com>
2025-09-02 14:07:34 +0100

    Update IWormhole.sol

--- contracts/src/interfaces/IWormhole.sol
+++ contracts/src/interfaces/IWormhole.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 interface IWormhole {
-    // VM is a struct that contains the data of a Wormhole message.
+    
     struct VM {
         uint8 version;
         uint32 timestamp;
@@ -14,17 +14,17 @@ interface IWormhole {
         bytes payload;
     }
 
-    // Returns the fee for publishing a message.
+    
     function messageFee() external view returns (uint256 fee);
 
-    // Publishes a message to the Wormhole network.
+    
     function publishMessage(
         uint32 nonce,
         bytes memory payload,
         uint8 consistencyLevel
     ) external payable returns (uint64 sequence);
 
-    // Parses and verifies a VAA. Reverts if the VAA is invalid.
+    
     function parseAndVerifyVM(
         bytes memory encodedVM
     ) external view returns (VM memory vm, bool valid, string memory reason);
